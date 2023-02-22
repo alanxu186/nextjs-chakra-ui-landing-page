803e8fb91dddf605f0d8e6088657913d55d04682
Alan Xu <alan.xu1699@gmail.com>
2023-02-22 14:01:30 -0500

    Create Testimonials.tsx file, import testimonials and features template from chakra-ui, set key prop to index in Testimonials.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,16 @@
-"use client"
+"use client";
 
-import HeroSection from "@/components/HeroSection"
+import Features from "@/components/Features";
+import HeroSection from "@/components/HeroSection";
+import Testimonials from "@/components/Testimonials";
+
 
 export default function Home() {
   return (
-    <HeroSection />
+    <>
+      <HeroSection />
+      <Features />
+      <Testimonials/>
+    </>
   )
 }
